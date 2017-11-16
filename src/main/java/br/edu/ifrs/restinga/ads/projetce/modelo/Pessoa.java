a8d0eb6cb85a737dcda533678fec1849cf2599b0
giordano <buffonggb@gmail.com>
2017-11-15 23:28:02 -0200

    Correcoes em Pessoa e Usuario

--- src/main/java/br/edu/ifrs/restinga/ads/projetce/modelo/Pessoa.java
+++ src/main/java/br/edu/ifrs/restinga/ads/projetce/modelo/Pessoa.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -17,74 +18,78 @@ import javax.persistence.InheritanceType;
 import javax.persistence.Transient;
 
 
-    @Entity
-    @Inheritance(strategy = InheritanceType.JOINED)
-    // Configurando herança 
-    @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, 
-        include=JsonTypeInfo.As.EXISTING_PROPERTY, property="tipo")
-    // define o tipo raiz 
-    @JsonTypeName("pessoa")
-    // tem que definir as subclasses conhecidas 
-    @JsonSubTypes({
-            @JsonSubTypes.Type(name="usuario", value=Usuario.class),
-            @JsonSubTypes.Type(name="administrador", value=Administrador.class)})
-    
-    
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+// Configurando herança
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "tipo")
+// define o tipo raiz
+@JsonTypeName("pessoa")
+// tem que definir as subclasses conhecidas
+@JsonSubTypes({
+        @JsonSubTypes.Type(name = "usuario", value = Usuario.class),
+        @JsonSubTypes.Type(name = "administrador", value = Administrador.class)})
+
+
 public abstract class Pessoa implements Serializable {
-    
+
     // para não gravar no banco 
     @Transient
     // Define o campo
     @JsonProperty("tipo")
     private final String tipo = "pessoa";
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    
-    @Column(unique = true, nullable = false, length=255)
+
+    @Column(unique = true, nullable = false, length = 255)
     private String email;
-    
-    @Column(nullable = false, length=255)
+
+    @Column(nullable = false, length = 255)
     private String senha;
-    
-    @Column(nullable = false, length=60)
+
+    @Column(nullable = false, length = 60)
     private String nome;
-    
-    @Column(length=12)
+
+    @Column(length = 15)
     private String telefone;
 
     public void setId(int id) throws Exception {
         if (id < 0)
             throw new Exception("Id deve ser maior que 1.");
         else
-            this.id = id;        
+            this.id = id;
     }
+
     public void setEmail(String email) throws Exception {
         if (email == null || email.isEmpty())
             throw new Exception("O campo E-mail é de preenchimento obrigatório!");
-        else if (new Utilitarios().validaEmail(email) == false)
+        else if (!new Utilitarios().validaEmail(email))
             throw new Exception("O E-mail digitado não é válido!");
         else
             this.email = email;
-       
+
     }
+
     public void setSenha(String senha) throws Exception {
         if (senha == null || senha.isEmpty())
             throw new Exception("O campo senha é de preenchimento obrigatório.");
         else
             this.senha = senha;
     }
+
     public void setNome(String nome) throws Exception {
         if (nome == null || nome.isEmpty())
             throw new Exception("O campo nome é de preenchimento obrigatório.");
         else if (nome.length() > 60)
             throw new Exception("Excedido o tamanho máximo para o campo nome");
-        else        
+        else
             this.nome = nome;
     }
+
     public void setTelefone(String telefone) throws Exception {
-        if (telefone.length() > 12)
+        if (telefone.length() > 15)
             throw new Exception("Excedido o tamanho máximo para o campo telefone");
         else
             this.telefone = telefone;
@@ -93,15 +98,19 @@ public abstract class Pessoa implements Serializable {
     public int getId() {
         return id;
     }
+
     public String getEmail() {
         return email;
     }
+
     public String getSenha() {
         return senha;
     }
+
     public String getNome() {
         return nome;
     }
+
     public String getTelefone() {
         return telefone;
     }
--- src/main/java/br/edu/ifrs/restinga/ads/projetce/modelo/Usuario.java
+++ src/main/java/br/edu/ifrs/restinga/ads/projetce/modelo/Usuario.java
@@ -4,10 +4,11 @@ package br.edu.ifrs.restinga.ads.projetce.modelo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Column;
- 
+
 import javax.persistence.Entity;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
@@ -21,57 +22,50 @@ import javax.persistence.Transient;
 @Entity
 //@PrimaryKeyJoinColumn(name="id")
 public class Usuario extends Pessoa {
-    
+
     // Para não gravar no banco 
     @Transient
     // Define o campo
     @JsonProperty("tipo")
     private final String tipo = "usuario";
-    
+
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
     private Date dataInsercao;
-    
+
     @JsonFormat(pattern = "yyyy-MM-dd")
     @Temporal(TemporalType.DATE)
     private Date dataDelecao;
-    
-    @Column(length=60)
+
+    @Column(length = 60)
     private String apelido;
-        
+
     @Lob
     private byte[] imagem;
-    
-    @Column(nullable = false, length=20)
-    private String tipoVinculo;
-    
-    @Column(nullable = false, columnDefinition = "varchar(80) default 'Não se aplica'", length=80) 
-    private String curso;
-    
-    
-    
-     
-    @OneToMany
-    private List<Area> areas;    
 
-     
+    @Column(nullable = false, length = 20)
+    private String tipoVinculo;
+
+    @Column(columnDefinition = "varchar(80) default 'Não se aplica'", length = 80)
+    private String curso;
+
+    @OneToMany
+    private List<Area> areas;
+
     @OneToMany(mappedBy = "coordenadorProjeto")
     private List<Projeto> projetosCoordenados;
-        
-     
+
+
     @ManyToMany(mappedBy = "integrantesProjeto")
     private List<Projeto> projetosIntegrados;
-    
-     
+
+
     @OneToMany(mappedBy = "promotorEvento")
     private List<Evento> eventosPromovidos;
 
-     
+
     @ManyToMany(mappedBy = "inscritosEvento")
     private List<Evento> eventosInscrito;
-
-
-
 
 
     public void setApelido(String apelido) throws Exception {
@@ -80,9 +74,11 @@ public class Usuario extends Pessoa {
         else
             this.apelido = apelido;
     }
+
     public void setImagem(byte[] imagem) {
         this.imagem = imagem;
     }
+
     public void setTipoVinculo(String tipoVinculo) throws Exception {
         if (tipoVinculo == null || tipoVinculo.isEmpty())
             throw new Exception("O campo Tipo de Vinculo é de preenchimento obrigatório.");
@@ -91,13 +87,16 @@ public class Usuario extends Pessoa {
         else
             this.tipoVinculo = tipoVinculo;
     }
+
     public void setCurso(String curso) throws Exception {
-        if (curso == null || curso.isEmpty())
-            throw new Exception("O campo curso é de preenchimento obrigatório.");
-        else if (tipoVinculo.length() > 80)
-            throw new Exception("Excedido o tamanho máximo para o campo curso");
-        else
-            this.curso = curso;
+        if (tipoVinculo.equalsIgnoreCase("aluno")) {
+            if (curso == null || curso.isEmpty())
+                throw new Exception("O campo curso é de preenchimento obrigatório.");
+            else if (tipoVinculo.length() > 80)
+                throw new Exception("Excedido o tamanho máximo para o campo curso");
+            else
+                this.curso = curso;
+        }
     }
 
     public void setProjetosCoordenados(List<Projeto> projetosCoordenados) {
@@ -107,31 +106,36 @@ public class Usuario extends Pessoa {
     public void setProjetosIntegrados(List<Projeto> projetosIntegrados) {
         this.projetosIntegrados = projetosIntegrados;
     }
+
     public void setEventosPromovidos(List<Evento> eventosPromovidos) {
         this.eventosPromovidos = eventosPromovidos;
     }
+
     public void setEventosInscrito(List<Evento> eventosInscrito) {
         this.eventosInscrito = eventosInscrito;
     }
-    
+
     public void setDataInsercao(Date dataInsercao) {
         this.dataInsercao = dataInsercao;
     }
+
     public void setDataDelecao(Date dataDelecao) {
         this.dataDelecao = dataDelecao;
     }
-    
 
 
     public String getApelido() {
         return apelido;
     }
+
     public byte[] getImagem() {
         return imagem;
     }
+
     public String getTipoVinculo() {
         return tipoVinculo;
     }
+
     public String getCurso() {
         return curso;
     }
@@ -139,26 +143,25 @@ public class Usuario extends Pessoa {
     public List<Projeto> getProjetosCoordenados() {
         return projetosCoordenados;
     }
+
     public List<Projeto> getProjetosIntegrados() {
         return projetosIntegrados;
     }
+
     public List<Evento> getEventosPromovidos() {
         return eventosPromovidos;
     }
+
     public List<Evento> getEventosInscrito() {
         return eventosInscrito;
     }
- 
+
     public Date getDataInsercao() {
         return dataInsercao;
     }
+
     public Date getDataDelecao() {
         return dataDelecao;
     }
- 
 
-
-
-
- 
 }
